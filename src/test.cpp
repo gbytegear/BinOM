1efdec460ab72e6c13e796b47c0bc6d256ee6ed1
Max Shemendiuk <shamanmax13@yandex.ru>
2021-09-13 07:25:38 +0300

    Some changes

--- src/test.cpp
+++ src/test.cpp
@@ -15,10 +15,44 @@ int main() {
 //    file_storage_multithreading_test();
 //    query_test();
 
-    Path path{1, "test", 2, "test_2", "test_3"};
-    std::clog << path.toString() << '\n';
-    Path path_2 = Path::fromString(path.toString());
-    std::clog << path_2.toString() << '\n';
+    Variable var = vobj{
+    {"usr", varr{
+      vobj{
+        {"login", "root"},
+        {"password", "root"},
+        {"access_lvl", 0xFF_ui8},
+        {"user_data", vobj{}},
+        {"attributes", varr{"full_access", "unchangeable"}}
+      },
+      vobj{
+        {"login", "guest"},
+        {"password", "guest"},
+        {"access_lvl", 0x00_ui8},
+        {"user_data", vobj{}},
+        {"attributes", varr{"ro_access", "unchangeable"}}
+      },
+      vobj{
+        {"login", "admin"},
+        {"password", "admin"},
+        {"access_lvl", 0x7f_ui8},
+        {"user_data", vobj{}},
+        {"attributes", varr{"unchangeable"}}
+      }
+    }},
+    {"grp", varr{
+      vobj{
+        {"name", "system"},
+        {"attributes", varr{"unremovable"}},
+        {"users", varr{"root", "guest", "admin"}}
+      }
+    }}
+  };
+    SerializedStorage s_storage("data.binom");
+    s_storage = var;
+    FileStorage f_storage("data.binomdb", var, true);
+
+    std::clog << "Serialized storage:\n" << s_storage << "\n\n"
+                 "File storage:\n" << f_storage.getRoot().getVariable() << "\n\n";
 
 
     std::clog << "=========================================================================\n"
